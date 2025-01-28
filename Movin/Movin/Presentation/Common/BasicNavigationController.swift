b3b1351495eaca7dbc73cfc1e013b35bd500f152
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-01-29 02:54:52 +0900

    feat: 공통 NavigationController 구현

---
+++ Movin/Movin/Presentation/Common/BasicNavigationController.swift
@@ -0,0 +1,28 @@
+//
+//  BasicNavigationController.swift
+//  Movin
+//
+//  Created by 조성민 on 1/28/25.
+//
+
+import UIKit
+
+final class BasicNavigationController: UINavigationController {
+    override init(rootViewController: UIViewController) {
+        super.init(rootViewController: rootViewController)
+        configureNavigationBar()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureNavigationBar() {
+        navigationBar.tintColor = .movinPrimary
+        navigationBar.barTintColor = .movinBlack
+        navigationBar.isTranslucent = false
+        navigationBar.titleTextAttributes = [
+            .foregroundColor: UIColor.movinWhite
+        ]
+    }
+}
