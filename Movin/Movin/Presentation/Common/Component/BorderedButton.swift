9bcbc9bd49107552c74db2c749048b89b34e85df
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-01-28 18:01:17 +0900

    feat: BorderedButton Component 구현

---
+++ Movin/Movin/Presentation/Common/Component/BorderedButton.swift
@@ -0,0 +1,30 @@
+//
+//  BorderedButton.swift
+//  Movin
+//
+//  Created by 조성민 on 1/28/25.
+//
+
+import UIKit
+
+//TODO: Bold Title 구현
+final class BorderedButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        configurate()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configurate() {
+        var buttonConfiguration = UIButton.Configuration.bordered()
+        buttonConfiguration.baseForegroundColor = .movinPrimary
+        buttonConfiguration.background.strokeWidth = 3
+        buttonConfiguration.background.strokeColor = .movinPrimary
+        buttonConfiguration.background.backgroundColor = .movinBlack
+        buttonConfiguration.cornerStyle = .capsule
+        self.configuration = buttonConfiguration
+    }
+}
