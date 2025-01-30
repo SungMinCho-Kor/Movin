50cad231a789229db6694f908129ba4a6ee4b081
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-01-30 20:53:21 +0900

    feat: Base Cell 구현

--- Movin/Movin/Presentation/Base/BaseTableViewCell.swift
+++ Movin/Movin/Presentation/Base/BaseTableViewCell.swift
@@ -1,18 +1,22 @@
 //
-//  BaseCollectionViewCell.swift
+//  BaseTableViewCell.swift
 //  Movin
 //
-//  Created by 조성민 on 1/28/25.
+//  Created by 조성민 on 1/30/25.
 //
 
 import UIKit
 
-class BaseCollectionViewCell: UICollectionViewCell, ViewConfiguration, ReusableIdentifier {
+class BaseTableViewCell: UITableViewCell, ViewConfiguration, ReusableIdentifier {
     
-    static var identifier: String { String(describing: self) }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    override init(
+        style: UITableViewCell.CellStyle,
+        reuseIdentifier: String?
+    ) {
+        super.init(
+            style: style,
+            reuseIdentifier: reuseIdentifier
+        )
         configureHierarchy()
         configureLayout()
         configureViews()
@@ -24,6 +28,8 @@ class BaseCollectionViewCell: UICollectionViewCell, ViewConfiguration, ReusableI
     }
     
     func configureHierarchy() { }
+    
     func configureLayout() { }
+    
     func configureViews() { }
 }
