c7033677895716b88aaea7a8721525702dfa7c69
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-02-01 03:20:13 +0900

    feat: 설정 프로필 편집 구현

--- Movin/Movin/Presentation/Profile/ProfileViewController.swift
+++ Movin/Movin/Presentation/Profile/ProfileViewController.swift
@@ -8,7 +8,46 @@
 import UIKit
 
 final class ProfileViewController: BaseViewController {
+    private let profileView = CinemaProfileView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func configureHierarchy() {
+        [
+            profileView
+        ].forEach(view.addSubview)
+    }
+    
+    override func configureLayout() {
+        profileView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.height.equalToSuperview().multipliedBy(0.2)
+        }
+    }
+    
+    override func configureViews() {
+        profileView.profileInfoButton.addTarget(
+            self,
+            action: #selector(profileInfoButtonTapped),
+            for: .touchUpInside
+        )
+    }
+    
+    @objc private func profileInfoButtonTapped() {
+        let profileEditViewController = ProfileEditViewController()
+        profileEditViewController.delegate = self
+        present(
+            BasicNavigationController(rootViewController: profileEditViewController),
+            animated: true
+        )
+    }
+}
+
+extension ProfileViewController: ProfileSaveDelegate {
+    func reloadProfile() {
+        profileView.refreshView()
     }
 }
